95df5a059ff3c6de79c1a279cae02241adcd29e9
haixiangyan <haixiang6123@gmail.com>
2022-11-26 09:23:12 +0800

    feat: 完成第二部分的基础样式

--- src/sections/SecondSection/index.tsx
+++ src/sections/SecondSection/index.tsx
@@ -1,9 +1,41 @@
 import {FC} from "react";
+import CartoonImage from '../../assets/cartoon.jpg';
+import MovieImage from '../../assets/movie.png';
+import LifeImage from '../../assets/life.jpg';
+import FoodImage from '../../assets/food.jpg';
+
+import styles from './styles.module.scss';
 
 const SecondSection: FC = () => {
   return (
-    <div>
-      Tabs + Tab Content
+    <div className={styles.secondSection}>
+      {/* Tabs */}
+      <ul>
+        <li>动画</li>
+        <li>美食</li>
+        <li>电影</li>
+        <li>生活</li>
+      </ul>
+
+      {/* Tab Content */}
+      <div>
+        <section>
+          <h2>动画</h2>
+          <img src={CartoonImage} alt="Cartoon"/>
+        </section>
+        <section>
+          <h2>美食</h2>
+          <img src={FoodImage} alt="Food"/>
+        </section>
+        <section>
+          <h2>电影</h2>
+          <img src={MovieImage} alt="Movie"/>
+        </section>
+        <section>
+          <h2>生活</h2>
+          <img src={LifeImage} alt="Life"/>
+        </section>
+      </div>
     </div>
   )
 }
